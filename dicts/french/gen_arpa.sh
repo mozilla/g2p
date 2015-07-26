09986a655b41eabf9c654e399dc52d8a8d45b1fc
Andre Natal <anatal@gmail.com>
2015-07-26 17:16:31 -0300

    Including French models and tools

---
+++ dicts/french/gen_arpa.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+../../bin/phonetisaurus-align --input=model.dic --ofile=model.corpus
+../../deps/srilm/bin/i686-m64/ngram-count -text model.corpus -lm model-incomplete.arpa
+perl normalize.pl > model.arpa
+sphinx_lm_convert -ifmt arpa -i model.arpa -o model.dic.dmp
+sphinx_lm_convert -i model.dic.dmp -o model.lm.bin
+
+rm model.corpus
+rm model.arpa
+rm model-incomplete.arpa
