7d0752a0e215f879b9dd171f6334d7c5f6dfcf01
Andre Natal <anatal@gmail.com>
2015-07-27 01:37:10 -0300

    Update models to match current Gecko sphinxbase version

--- dicts/english/gen_arpa.sh
+++ dicts/english/gen_arpa.sh
@@ -1,10 +1,9 @@
 #/bin/bash
 
 ../../bin/phonetisaurus-align --input=model.dic --ofile=model.corpus
-../../deps/srilm/bin/i686-m64/ngram-count -text model.corpus -lm model-incomplete.arpa
-perl normalize.pl > model.arpa
+../../deps/srilm/bin/i686-m64/ngram-count -text model.corpus -lm model-unsorted.arpa
+sphinx_lm_sort < model-unsorted.arpa > model.arpa
 sphinx_lm_convert -ifmt arpa -i model.arpa -o model.dic.dmp
-sphinx_lm_convert -i model.dic.dmp -o model.lm.bin
+#sphinx_lm_convert -i model.dic.dmp -o model.lm.bin ##  Only works with lm trie sphinxbase version
 
-rm model.corpus
-rm model-incomplete.arpa
+rm model-unsorted.arpa
